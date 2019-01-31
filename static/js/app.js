832a53fc985ba52764d654367c69ef0781dab4dd
hzhou365 <hzhou365@gmail.com>
2019-01-30 20:19:26 -0800

    Initial revision

---
+++ static/js/app.js
@@ -0,0 +1,40 @@
+// from data.js
+var tableData = data;
+
+// YOUR CODE HERE!
+
+var tbody = d3.select("tbody");
+
+function show(data) {
+  data.forEach((UFOReport) => {
+    var row = tbody.append("tr");
+    Object.entries(UFOReport).forEach(([key, value]) => {
+      var cell = tbody.append("td");
+      cell.text(value);
+    });
+  });
+}
+
+var submit = d3.select("#filter-btn");
+
+submit.on("click", function() {
+
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+
+  console.log(inputValue);
+  console.log(tableData[0]);
+
+  var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+  console.log(filteredData);
+
+  show(filteredData);
+
+});
